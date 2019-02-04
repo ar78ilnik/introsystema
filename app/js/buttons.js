a6ffa122b7803a53c50f922d2d06794e7b0a6a27
ar78ilnik <arakchiev.ilya@yandex.ru>
2019-02-04 21:40:24 +0300

    4-th feb

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -1,4 +1,1 @@
-//var articles = document.getElementsByClassName("article");
-for (var i = 0; i < document.body.childNodes.length; i++) {
-    alert (document.body.childNodes[i]);
-}
+var articles = document.getElementsByClassName("article");
