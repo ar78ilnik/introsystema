9eec6c4cd406d994ae16951fe4dbcf3622e557bb
ar78ilnik <arakchiev.ilya@yandex.ru>
2019-02-07 22:14:25 +0300

    7-th feb

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -1,1 +1,12 @@
-var articles = document.getElementsByClassName("article");
+/*var articles = document.getElementsByClassName("article");
+var slogans = document.getElementsByClassName("slogan__link");
+var i=0;
+slogans.classList("slogan__link")[i].onclick = function () {
+    articles.classList("article")[i].style.display = "block";
+}*/
+
+document.querySelectorAll('.slogan__link').forEach(i => i.onclick = event => {
+    let a = event.currentTarget.getAttribute('href')
+    document.querySelectorAll('article').forEach(i => i.style.display = 'none')
+    document.querySelector(a).closest('article').style.display = 'block'
+})
